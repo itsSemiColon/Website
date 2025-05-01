a177dc0cca24ca0138845b338571f00a2ebc5028
Muhammad Ibtasam <its.semicolon.io@gmail.com>
2025-05-01 23:11:50 +0500

    adding footer section

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -1,9 +1,38 @@
-// import React from 'react'
+import styles from './styles/footer.module.scss';
 
-const footer = () => {
+const Footer = () => {
   return (
-    <div></div>
-  )
-}
+    <footer className={styles['footer-wrapper']}>
+      <div className={styles['footer-container']}>
+        <div className={styles['footer-top']}>
+          <a href="#" className={styles['footer-brand']}>
+          <img
+            decoding="async"
+            width="100"
+            height="286"
+            sizes="105px"
+            srcSet="https://framerusercontent.com/images/LtJOacjpCfrAdQJhzULOxsNXP8.png?scale-down-to=512 512w,https://framerusercontent.com/images/LtJOacjpCfrAdQJhzULOxsNXP8.png 768w"
+            src="https://framerusercontent.com/images/LtJOacjpCfrAdQJhzULOxsNXP8.png?scale-down-to=512"
+            alt=""
+          />
+          </a>
 
-export default footer
+          <ul className={styles['footer-links']}>
+            <li><a href="#">Services</a></li>
+            <li><a href="#">Industries</a></li>
+            <li><a href="#">Company</a></li>
+            <li><a href="#">Contact</a></li>
+          </ul>
+        </div>
+
+        <hr className={styles['footer-divider']} />
+
+        <p className={styles['footer-bottom-text']}>
+          © 2023 <a href="#" className={styles['footer-bottom-link']}>Semicolon™</a>. All Rights Reserved.
+        </p>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
